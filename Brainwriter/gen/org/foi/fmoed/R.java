12533c6b37daf43eaf183db98aa1c7f2c372f8c8
epson121 <lurajcevi@gmail.com>
2013-09-18 17:23:48 +0200

    cmt

--- Brainwriter/gen/org/foi/fmoed/R.java
+++ Brainwriter/gen/org/foi/fmoed/R.java
@@ -24,10 +24,17 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080005;
+        public static final int group_list=0x7f080004;
+        public static final int group_list_layout=0x7f080003;
+        public static final int pager=0x7f080000;
+        public static final int pager_title_strip=0x7f080001;
+        public static final int section_label=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int fragment_main=0x7f030001;
+        public static final int groups_list=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -36,6 +43,9 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int title_section1=0x7f050003;
+        public static final int title_section2=0x7f050004;
+        public static final int title_section3=0x7f050005;
     }
     public static final class style {
         /** 
---
+++ Brainwriter/src/org/foi/fmoed/adapters/GroupAdapter.java
@@ -0,0 +1,49 @@
+package org.foi.fmoed.adapters;
+
+import org.foi.fmoed.R;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class GroupAdapter extends BaseAdapter{
+	
+	private Context con;
+	
+	public GroupAdapter(Context c) {
+		this.con = c;
+	}
+	
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return 5;
+	}
+
+	@Override
+	public Object getItem(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getItemId(int arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		final int pos = position;
+		View v = convertView;
+		LayoutInflater li;
+		if (convertView == null){
+				li = LayoutInflater.from(con);
+				v = li.inflate(R.layout.groups_list, null);
+		}
+		return v;
+	}
+
+}
