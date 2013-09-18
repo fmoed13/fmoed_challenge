74adc34cc7bdc2f07c4e940d8961de98310a4d3a
ivpusic <pusic007@gmail.com>
2013-09-18 23:49:14 +0200

    merged

--- Brainwriter/gen/org/foi/fmoed/R.java
+++ Brainwriter/gen/org/foi/fmoed/R.java
@@ -24,15 +24,17 @@ public final class R {
         public static final int add_group=0x7f020000;
         public static final int camera=0x7f020001;
         public static final int canvas=0x7f020002;
-        public static final int clock=0x7f020003;
-        public static final int fav=0x7f020004;
-        public static final int group=0x7f020005;
-        public static final int ic_launcher=0x7f020006;
-        public static final int search=0x7f020007;
-        public static final int start=0x7f020008;
-        public static final int start1=0x7f020009;
-        public static final int text=0x7f02000a;
-        public static final int text1=0x7f02000b;
+        public static final int canvas_icon=0x7f020003;
+        public static final int clock=0x7f020004;
+        public static final int fav=0x7f020005;
+        public static final int group=0x7f020006;
+        public static final int ic_launcher=0x7f020007;
+        public static final int search=0x7f020008;
+        public static final int start=0x7f020009;
+        public static final int start1=0x7f02000a;
+        public static final int text=0x7f02000b;
+        public static final int text1=0x7f02000c;
+        public static final int text_icon=0x7f02000d;
     }
     public static final class id {
         public static final int action_settings=0x7f080014;
